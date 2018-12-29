b4540d06f5ed6ea894ec2deda685de2c66a62e04
khushboo <khushboo.v@dynproindia.com>
2018-12-29 23:15:37 +0530

    add radio button action for P,A,L

--- src/app/attendance/attendance.component.ts
+++ src/app/attendance/attendance.component.ts
@@ -10,25 +10,21 @@ import { RestService } from '../rest.service';
 })
 export class AttendanceComponent {
 
-  classData = CLASSES;
-displayedColumns = ['roll_number','first_name','last_name','present','absent','leave'];
+classData = CLASSES;
 classId;
 studentData: any = [];
-present: string = 'P';
-absent: string = 'A';
-leave: string = 'L';
-
 
 constructor(public rest:RestService){
  console.log("class:",this.classData);	
 };
 
 public getClassName(id): void {  
-    console.log("class id: "+id);
     let classObj = {"fn": "selectAllById","params": ["students",['class'],[id] ]};
     this.rest.getStudentsById(classObj).subscribe((response) => {
-		 console.log("res KV: ",response);
 		 this.studentData = response;
+    	 for(let i=0; i<((this.studentData).length ); i++){
+           this.studentData[i].action = 'present';
+		 }
 	});
 }
 
@@ -39,51 +35,12 @@ public getDate(date): void {
 submitAttendance(form:NgForm){
 	console.log("att form: ",form);
 	 let keys = Object.keys(form.controls);
-    //form.value.dob = (form.value.dob).toString();
     let values = Object.values(form.value);
-    //let classValue = Object.values(form.value.class) ;
     console.log("keys: ",keys);
     console.log("values: ",values);
 }
+
+onRadioClick(index,val) {
+    this.studentData[index].action = val;
+  }
 }
-
-/*
-import { Component, OnChanges } from '@angular/core';
-import { CLASSES } from '../class';
-import { FormControl,NgForm,Validators } from '@angular/forms';
-import { RestService } from '../rest.service';
-
-@Component({
-	selector:'app-attendance',
-	templateUrl:'./attendance.component.html',
-	 styleUrls: ['./attendance.component.css']
-})
-
-export class AttendanceComponent implements OnChanges {
-
-classData = CLASSES;
-displayedColumns = ['roll_number','first_name','last_name','present','absent','leave'];
-classId;
-studentData;
-
-constructor(public rest:RestService){
- console.log("class:",this.classData);	
-};
-
-public getClassName(id): void {  
-    console.log("class id: "+id);
-    let classObj = {"fn": "selectAllById","params": ["students",['class'],[id]] };
-    this.rest.getStudentsById(classObj).subscribe((response) => {
-		 console.log("res KV: ",response);
-		 this.studentData = response;
-	});
-}
-
-public getDate(date): void {
-	console.log("date:",date);
-}
-
-submitAttendance(form:NgForm){
-	console.log("att form: ",form);
-}
-}*/
