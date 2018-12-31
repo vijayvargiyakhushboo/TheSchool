6d9dac30facacc18fa17fb38a2b7b0c6fcbd9b28
khushboo <khushboo.v@dynproindia.com>
2018-12-31 23:06:38 +0530

    add list attendance

--- src/app/attendance/attendanceView.component.ts
+++ src/app/attendance/attendanceView.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit,OnChanges } from '@angular/core';
-
-
+import { RestService } from '../rest.service';
+import { CLASSES } from '../class';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-attendance-view',
@@ -8,6 +9,22 @@ import { Component, OnInit,OnChanges } from '@angular/core';
   styleUrls: ['./attendance.component.css']
 })
 export class AttendanceViewComponent {
-constructor(){};
+    classData = CLASSES;
+	classId;
+	dataSource;
+	displayedColumns = ['roll_number','first_name','last_name','action'];
+	todayDate = this.datePipe.transform(new Date(), 'yyyy-MM-dd');
+constructor(private rest:RestService,private datePipe:DatePipe){};
+public getClassName(id): void {
+this.classId = id;  
+    let classObj = { "fn": "selectAttendance","params": ["attendance","students",[this.classId],[this.todayDate] ]};
+    this.rest.getAttendanceByClass(classObj).subscribe((response) => {
+		 this.dataSource = response;
+		 console.log("res: ",response);
+    	 /*for(let i=0; i<((this.studentData).length ); i++){
+           this.studentData[i].action = 'present';
+		 }*/
+	});
+}
 
 }
