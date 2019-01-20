5762a58db58cf9ad8f5c6456023809787c88ca53
khushboo <khushboo.v@dynproindia.com>
2019-01-21 00:15:10 +0530

    fee add

--- src/app/receipt/receipt.component.ts
+++ src/app/receipt/receipt.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { RestService} from '../rest.service';
-import { FormControl,NgForm,Validators } from '@angular/forms';
+import { FormControl,Validators,FormGroup } from '@angular/forms';
 import { CLASSES } from '../class';
 import { Router } from '@angular/router';
 import { DatePipe } from '@angular/common';
@@ -14,12 +14,49 @@ import { DatePipe } from '@angular/common';
 export class ReceiptComponent implements OnInit {
     studentData: any = {};
 	classData = CLASSES ;
+	form : FormGroup;
 	todayDate = this.datePipe.transform(new Date(), 'yyyy-MM-dd');
   constructor(public datePipe : DatePipe) { 
   	console.log("date: "+(new Date()) );
   }
 
   ngOnInit() {
+  	this.form = new FormGroup({
+  		'session' : new FormControl('',{
+  			validators:[Validators.required]
+  		}),
+  		'student_name' : new FormControl('',{
+  			validators:[Validators.required]
+  		}),
+  		'mother_name' : new FormControl('',{
+  			validators:[Validators.required]
+  		}),
+  		'father_name' : new FormControl('',{
+  			validators:[Validators.required]
+  		}),
+  		'class' : new FormControl('',{
+  			validators:[Validators.required]
+  		}),
+  		'adm_fee' : new FormControl('',{
+  			validators:[Validators.required]
+  		}),
+  		'dearness_fee' : new FormControl('',{
+  			validators:[Validators.required]
+  		}),
+  		'total_amount' : new FormControl('',{
+  			validators:[Validators.required]
+  		}),
+  		'amt_deposite' : new FormControl('',{
+  			validators:[Validators.required]
+  		})
+  	})
+  }
+
+  saveStudentFee(){
+  	let keys = Object.keys(this.form.controls);
+  	let values = Object.values(this.form.value);
+  	console.log("keys: ",keys);
+  	console.log("values: ",values);
   }
 
 }
