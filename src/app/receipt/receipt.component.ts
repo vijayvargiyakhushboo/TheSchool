02b9802b98a3c5fd52ee8fc10d6b582eae936152
khushboo <khushboo.v@dynproindia.com>
2019-02-01 23:57:45 +0530

    add amount deposite

--- src/app/receipt/receipt.component.ts
+++ src/app/receipt/receipt.component.ts
@@ -16,36 +16,25 @@ export class ReceiptComponent implements OnInit {
 	classData = CLASSES ;
   sum;
 
-	//form : FormGroup;
 	todayDate = this.datePipe.transform(new Date(), 'yyyy-MM-dd');
-
-   
-
   form: FormGroup;
   constructor(public datePipe : DatePipe) { 
   	console.log("date: "+(new Date()) );
-   
-    
-   
-    
-
-
-  }
+    }
 
   ngOnInit() {
-  	
-     
-      
   }
 
   saveStudentFee(form: NgForm){
-    // this.form.get('tution_fee').clearValidators();
     if(form.invalid){
     return;
     }
-
+let deposit_amt = +(form.controls.pre_bal.value ) + (form.controls.form_chrgs.value) + (form.controls.adm_fee.value) + (form.controls.dearness_fee.value)
+ + (form.controls.elec_fee.value) + (form.controls.lib_fee.value) + (form.controls.lab_fee.value) + (form.controls.music_fee.value) 
+ + (form.controls.annual_func_fee.value) + (form.controls.exam_fee.value) + (form.controls.sclass.value) + (form.controls.tution_fee.value)  + (form.controls.acti_fee.value)  + (form.controls.late_fee.value)  ;
   	let keys = Object.keys(form.controls);
-  	let values = Object.values(form.value);
+    form.value.amt_deposite= deposit_amt;
+    let values = Object.values(form.value);
   	console.log("keys: ",keys);
   	console.log("values: ",values);
   }
