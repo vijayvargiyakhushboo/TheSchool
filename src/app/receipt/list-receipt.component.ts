8328619ccb44e237070d127bb3baaa37ff95a008
khushboo <khushboo.v@dynproindia.com>
2019-02-10 11:21:13 +0530

    add sort on receipt list

--- src/app/receipt/list-receipt.component.ts
+++ src/app/receipt/list-receipt.component.ts
@@ -1,6 +1,11 @@
-import { Component, OnInit,Inject } from '@angular/core';
+import { Component, OnInit,Inject,ViewChild } from '@angular/core';
 import { RestService } from '../rest.service';
+import { MatDialog, MAT_DIALOG_DATA ,MatTableDataSource,MatSort } from '@angular/material';
+import { Router } from '@angular/router';
 
+export interface DialogData {
+  receiptId;
+}
 
 @Component({
   selector: 'app-list-receipt',
@@ -10,15 +15,52 @@ import { RestService } from '../rest.service';
 export class ListReceiptComponent implements OnInit {
 	dataSource ;
 displayedColumns = ['session','class','student_name','father_name','mother_name','total_amount','amt_deposite','pre_bal','bal_amount','annual_func_fee','dearness_fee','dev_fee','elec_fee','exam_fee','form_chrgs','lab_fee','lib_fee','monthName','music_fee','received_by','remark','sclass','searchDate','uId'];
-constructor(public rest:RestService){
-this.rest.getReceipt().then((response) => {
+constructor(public rest:RestService, public dialog: MatDialog){
+
+}
+
+openDialog(receiptData) {
+   console.log("receiptData console: ",receiptData);
+    const dialogRef = this.dialog.open(DialogContent, {
+      data: {
+        receiptId: receiptData.uId
+      }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+      console.log("Dialog result: ",result);
+    });
+  }
+
+  @ViewChild(MatSort) sort: MatSort;
+ngOnInit() {
+  this.rest.getReceipt().then((response) => {
     console.log("res KV: ",response);
     this.dataSource = response ;
-   
     console.log("dataSource receipt :",this.dataSource);
+    this.dataSource = new MatTableDataSource(response);
+    this.dataSource.sort = this.sort;
 });
+  }
 }
-ngOnInit() {
+
+@Component({
+  selector: 'dialog-content-example-dialog',
+  templateUrl: 'delete_receipt_dialog.html',
+})
+export class DialogContent {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: DialogData,public rest: RestService,private router: Router) {}
+  deleteReceipt(id) {
+   console.log("delete receipt : "+id);
+     this.rest.delete("receipt",id).then((response) => {
+       alert("receipt deleted.");
+       this.rest.getReceipt().then((response) => {
+    console.log("res KV getReceipt: ",response);
+
+});
+       this.router.navigate(['/receiptList']);
+    });
   }
 }
 
